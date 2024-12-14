a05ce100a33953dbe1e59a2c993b626f0fe9a617
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2024-12-14 18:01:16 +0000

    Tidy day13

--- day13.py
+++ day13.py
@@ -5,51 +5,44 @@ import re
 from itertools import batched
 import numpy as np
 
-r = re.compile(r'\d+')
+def solve(a, b, p, part2=False):
 
+    (ax, ay) = a
+    (bx, by) = b
+
+    m = np.array([[ax, bx], [ay, by]])
+
+    p = np.array(p)
+
+    if part2:
+        p[0] += 10000000000000
+        p[1] += 10000000000000
+
+    sol = np.linalg.solve(m, p)
+
+    sol = np.fromiter(map(round, sol), dtype=int)
+
+    # pp is where we end up
+    pp = m.dot(np.array(sol))
+
+    if not np.any(pp - p):
+        return sol.dot([3, 1])
+    else:
+        return 0
+
+
+r = re.compile(r'\d+')
 def generate_lines():
     for line in fileinput.input():
         x = re.findall(r, line)
         if x:
             yield [int(y) for y in x]
 
-in_data = list(batched(generate_lines(), 3))
-
-def solve(part2=False):
-
-    ret = 0
-
-    for (ax, ay), (bx, by), (px, py) in in_data:
-
-        m = np.array([[ax, bx], [ay, by]])
-
-        p = np.array([px, py])
-
-        if part2:
-            p[0] += 10000000000000
-            p[1] += 10000000000000
-
-        sol = np.linalg.solve(m, p)
-
-        ra = round(sol[0])
-        rb = round(sol[1])
-
-        ppx = ax * ra + bx * rb
-        ppy = ay * ra + by * rb
-
-        if ppx - p[0] == 0 and  ppy - p[1] == 0:
-            price = ra * 3 + rb
-            ret += price
-        else:
-            price = -1
-
-        # print(f"{ax} {ay} {bx} {by} : {p[0]} {p[1]} ---> {price}")
-
-    return ret
-
-
-part1 = solve()
-part2 = solve(part2=True)
+part1 = 0
+part2 = 0
+for data in batched(generate_lines(), 3):
+    part1 += solve(*data)
+    part2 += solve(*data, part2=True)
 
 print("Part 1:", part1)
 print("Part 2:", part2)
